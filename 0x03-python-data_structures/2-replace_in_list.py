4980b3512f20cd461fc9a41cfcb8edd70188a803
kaydeemoney <daramola4082@gmail.com>
2024-01-05 23:31:12 -0800

    hope this pass checker

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list) - 1:
-        return 'None'
+
+def replace_in_list(my_list, idx, element):
+    n = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx > n - 1:
+        return my_list
     else:
-        return my_list[idx]
+        my_list[idx] = element
+        return my_list
